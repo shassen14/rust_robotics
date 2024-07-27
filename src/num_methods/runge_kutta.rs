5280100fff69e75660f905e9e217e796bb174ddb
shassen14 <samirhassen14@gmail.com>
2024-07-27 17:49:06 -0500

    changed pass by reference for vectors and documented functions

--- src/num_methods/runge_kutta.rs
+++ src/num_methods/runge_kutta.rs
@@ -1,128 +1,179 @@
 extern crate nalgebra as na;
 
 #[allow(dead_code)]
-// Generic function pointer alias which represents dx/dt = f(x,t)
-type VectorFn<T, const R: usize> = fn(na::SVector<T, R>, T) -> na::SVector<T, R>;
+/// Generic function pointer alias which represents dx/dt = f(x,t)
+type VectorFn<T, const R: usize> = fn(&na::SVector<T, R>, T) -> na::SVector<T, R>;
 
 #[allow(dead_code)]
-// Euler method
-// This is stating for Type T: Subtracting another T will give an output of T
-// Type T: Multiplying with na::SVector will return a SVector
-// Type T: Allowing copying in order to input into func
-// where SVector: added with another SVector will output SVector
+/// Integrate a function, dx/dt = func(x, t), using Runge-Kutta 1st order (Euler) for a single timestep
+///
+/// # Arguments
+///
+/// * `func` - Function to integrate
+/// * `x0` - Initital states x(t0)
+/// * `t0` - Initial time
+/// * `tf` - Final time
+/// * Returns Final State x(tf)
+///
 fn rk1<T, const R: usize>(
     func: VectorFn<T, R>,
-    x0: na::SVector<T, R>,
+    x0: &na::SVector<T, R>,
     t0: T,
     tf: T,
 ) -> na::SVector<T, R>
 where
-    T: std::ops::Sub<Output = T>
-        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>
-        + Copy,
-    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>,
+    T: std::ops::Sub<Output = T> // T - T = T
+        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>> // T * Vec = Vec
+        + Copy, // T is copyable
+    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>, // Vec + Vec = Vec
 {
     let dt: T = tf - t0;
-    x0 + dt * func(x0, t0)
+    *x0 + dt * func(x0, t0)
 }
 
 #[allow(dead_code)]
+/// Integrate a function, dx/dt = func(x, t), using Runge-Kutta 2nd order (midpoint) for a single timestep
+///
+/// # Arguments
+///
+/// * `func` - Function to integrate
+/// * `x0` - Initital states x(t0)
+/// * `t0` - Initial time
+/// * `tf` - Final time
+/// * Returns Final State x(tf)
+///
 fn rk2<T, const R: usize>(
     func: VectorFn<T, R>,
-    x0: na::SVector<T, R>,
+    x0: &na::SVector<T, R>,
     t0: T,
     tf: T,
 ) -> na::SVector<T, R>
 where
-    T: std::ops::Sub<Output = T>
-        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>
-        + std::ops::Add<Output = T>
-        + Copy,
-    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>,
-    f64: std::ops::Mul<T, Output = T>,
+    T: std::ops::Sub<Output = T> // T - T = T
+        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>> // T * Vec = Vec
+        + std::ops::Add<Output = T> // T + T = T
+        + Copy, // T is copyable
+    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>, // Vec + Vec = Vec
+    f64: std::ops::Mul<T, Output = T>,                                               // f64 * T = T
 {
     let dt: T = tf - t0;
     let k1: na::SVector<T, R> = func(x0, t0);
-    let k2: na::SVector<T, R> = func(x0 + dt * k1, t0 + dt);
-    x0 + 0.5 * dt * (k1 + k2)
+    let k2: na::SVector<T, R> = func(&(*x0 + dt * k1), t0 + dt);
+    *x0 + 0.5 * dt * (k1 + k2)
 }
 
 #[allow(dead_code)]
+/// Integrate a function, dx/dt = func(x, t), using Runge-Kutta 3rd order for a single timestep
+///
+/// # Arguments
+///
+/// * `func` - Function to integrate
+/// * `x0` - Initital states x(t0)
+/// * `t0` - Initial time
+/// * `tf` - Final time
+/// * Returns Final State x(tf)
+///
 fn rk3<T, const R: usize>(
     func: VectorFn<T, R>,
-    x0: na::SVector<T, R>,
+    x0: &na::SVector<T, R>,
     t0: T,
     tf: T,
 ) -> na::SVector<T, R>
 where
-    T: std::ops::Sub<Output = T>
-        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>
-        + std::ops::Add<Output = T>
-        + std::ops::Div<f64, Output = T>
-        + Copy,
-    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>,
-    f64:
-        std::ops::Mul<T, Output = T> + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>,
+    T: std::ops::Sub<Output = T> // T - T = T
+        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>> // T * Vec = Vec
+        + std::ops::Add<Output = T> // T + T = T
+        + std::ops::Div<f64, Output = T> // T / f64 = T
+        + Copy, // T is copyable
+    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>, // Vec + Vec = Vec
+    f64: std::ops::Mul<T, Output = T> // f64 * T = T
+        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>, // f64 * Vec = Vec
 {
     let dt: T = tf - t0;
     let k1: na::SVector<T, R> = func(x0, t0);
-    let k2: na::SVector<T, R> = func(x0 + dt * k1, t0 + dt);
-    let k3: na::SVector<T, R> = func(x0 + (dt / 4.) * (k1 + k2), t0 + dt / 2.);
+    let k2: na::SVector<T, R> = func(&(*x0 + dt * k1), t0 + dt);
+    let k3: na::SVector<T, R> = func(&(*x0 + (dt / 4.) * (k1 + k2)), t0 + dt / 2.);
 
-    x0 + (dt / 6.) * (k1 + k2 + 4. * k3)
+    *x0 + (dt / 6.) * (k1 + k2 + 4. * k3)
 }
 
 #[allow(dead_code)]
+/// Integrate a function, dx/dt = func(x, t), using Runge-Kutta 4th order for a single timestep
+///
+/// # Arguments
+///
+/// * `func` - Function to integrate
+/// * `x0` - Initital states x(t0)
+/// * `t0` - Initial time
+/// * `tf` - Final time
+/// * Returns Final State x(tf)
+///
 fn rk4<T, const R: usize>(
     func: VectorFn<T, R>,
-    x0: na::SVector<T, R>,
+    x0: &na::SVector<T, R>,
     t0: T,
     tf: T,
 ) -> na::SVector<T, R>
 where
-    T: std::ops::Sub<Output = T>
-        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>
-        + std::ops::Div<f64, Output = T>
-        + std::ops::Mul<f64, Output = T>
-        + std::ops::Add<Output = T>
-        + Copy,
-    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>,
-    f64:
-        std::ops::Mul<T, Output = T> + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>,
+    T: std::ops::Sub<Output = T> // T - T = T
+        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>> // T * Vec = Vec
+        + std::ops::Div<f64, Output = T> // T / f64 = T
+        + std::ops::Mul<f64, Output = T> // T * f64 = T
+        + std::ops::Add<Output = T> // T + T = T
+        + Copy, // T is copyable
+    na::SVector<T, R>: std::ops::Add<na::SVector<T, R>, Output = na::SVector<T, R>>, // Vec + Vec = Vec
+    f64: std::ops::Mul<T, Output = T> // f64 * T = T
+        + std::ops::Mul<na::SVector<T, R>, Output = na::SVector<T, R>>, // f64 * Vec = Vec
 {
     let dt: T = tf - t0;
     let k1: na::SVector<T, R> = func(x0, t0);
-    let k2: na::SVector<T, R> = func(x0 + (dt / 2.) * k1, t0 + dt / 2.);
-    let k3: na::SVector<T, R> = func(x0 + (dt / 2.) * k2, t0 + dt / 2.);
-    let k4: na::SVector<T, R> = func(x0 + dt * k3, tf);
+    let k2: na::SVector<T, R> = func(&(*x0 + (dt / 2.) * k1), t0 + dt / 2.);
+    let k3: na::SVector<T, R> = func(&(*x0 + (dt / 2.) * k2), t0 + dt / 2.);
+    let k4: na::SVector<T, R> = func(&(*x0 + dt * k3), tf);
 
-    x0 + (dt / 6.) * (k1 + 2. * k2 + 2. * k3 + k4)
+    *x0 + (dt / 6.) * (k1 + 2. * k2 + 2. * k3 + k4)
 }
+
+///////////////////////////////////////////////////////////////////////////////
+/// Tests
+///////////////////////////////////////////////////////////////////////////////
 #[cfg(test)]
 mod tests {
     use super::*;
 
     // Alias for integrator to use
     type IntegrationFn<const R: usize> =
-        fn(VectorFn<f64, R>, na::SVector<f64, R>, f64, f64) -> na::SVector<f64, R>;
+        fn(VectorFn<f64, R>, &na::SVector<f64, R>, f64, f64) -> na::SVector<f64, R>;
 
-    // Helper function to check the results from integrations
+    /// Helper function to est integrating a function over a time interval [start, end]
+    ///
+    /// # Arguments
+    ///
+    /// * `integrator` - Integrator to use
+    /// * `func` - Function to integrate
+    /// * `start` -  Start time for integration
+    /// * `end` - Final time for integration
+    /// * `step` - Step size for integration
+    /// * `expected_result` - Expected result of integration [start, final]
+    /// * `max_error` - Maximum allowed error between final result and the expected result
+    /// * Returns Final State x(tf)
+    ///
     fn test_integration<const R: usize>(
         integrator: IntegrationFn<R>,
         func: VectorFn<f64, R>,
-        x0: na::SVector<f64, R>,
+        x0: &na::SVector<f64, R>,
         start: f64,
         end: f64,
         step: f64,
-        expected_result: na::SVector<f64, R>,
-        max_error: na::SVector<f64, R>,
+        expected_result: &na::SVector<f64, R>,
+        max_error: &na::SVector<f64, R>,
     ) -> na::SVector<f64, R> {
         let mut t0: f64 = start;
         let mut tf: f64 = start + step;
-        let mut result: na::SVector<f64, R> = x0;
+        let mut result: na::SVector<f64, R> = *x0;
 
         while tf <= end {
-            result = integrator(func, result, t0, tf);
+            result = integrator(func, &result, t0, tf);
             t0 = tf;
             tf += step;
         }
@@ -145,7 +196,16 @@ mod tests {
 
     ///////////////////////////////////////////////////////////////////////////
 
-    // cos test
+    /// Test integrating a cos(t) over a time interval [start, end]
+    ///
+    /// # Arguments
+    ///
+    /// * `integrator` - Integrator to use
+    /// * `start` -  Start time for integration
+    /// * `end` - Final time for integration
+    /// * `step` - Step size for integration
+    /// * `max_error` - Maximum allowed error between final result and the expected result
+    ///
     fn test_cos(
         integrator: IntegrationFn<1>,
         start: f64,
@@ -153,24 +213,31 @@ mod tests {
         step: f64,
         max_error: f64,
     ) -> () {
-        let func = |_: na::SVector<f64, 1>, t: f64| na::SVector::<f64, 1>::new(f64::cos(t));
+        let func = |_: &na::SVector<f64, 1>, t: f64| na::SVector::<f64, 1>::new(f64::cos(t));
 
         let expected = f64::sin(end) - f64::sin(start);
         test_integration(
             integrator,
             func,
-            na::SVector::<f64, 1>::new(0.),
+            &na::SVector::<f64, 1>::new(0.),
             start,
             end,
             step,
-            na::SVector::<f64, 1>::new(expected),
-            na::SVector::<f64, 1>::new(max_error),
+            &na::SVector::<f64, 1>::new(expected),
+            &na::SVector::<f64, 1>::new(max_error),
         );
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-
-    // sin test
+    /// Test integrating a sin(t) over a time interval [start, end]
+    ///
+    /// # Arguments
+    ///
+    /// * `integrator` - Integrator to use
+    /// * `start` -  Start time for integration
+    /// * `end` - Final time for integration
+    /// * `step` - Step size for integration
+    /// * `max_error` - Maximum allowed error between final result and the expected result
+    ///
     fn test_sin(
         integrator: IntegrationFn<1>,
         start: f64,
@@ -178,23 +245,31 @@ mod tests {
         step: f64,
         max_error: f64,
     ) -> () {
-        let func = |_: na::SVector<f64, 1>, t: f64| na::SVector::<f64, 1>::new(f64::sin(t));
+        let func = |_: &na::SVector<f64, 1>, t: f64| na::SVector::<f64, 1>::new(f64::sin(t));
 
         let expected = -f64::cos(end) - -f64::cos(start);
         test_integration(
             integrator,
             func,
-            na::SVector::<f64, 1>::new(0.),
+            &na::SVector::<f64, 1>::new(0.),
             start,
             end,
             step,
-            na::SVector::<f64, 1>::new(expected),
-            na::SVector::<f64, 1>::new(max_error),
+            &na::SVector::<f64, 1>::new(expected),
+            &na::SVector::<f64, 1>::new(max_error),
         );
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-
+    /// Test integrating a constant velocity system over a time interval [start, end]
+    ///
+    /// # Arguments
+    ///
+    /// * `integrator` - Integrator to use
+    /// * `start` -  Start time for integration
+    /// * `end` - Final time for integration
+    /// * `step` - Step size for integration
+    /// * `max_error` - Maximum allowed error between final result and the expected result
+    ///
     fn test_cv(
         integrator: IntegrationFn<2>,
         x0: f64,
@@ -202,9 +277,9 @@ mod tests {
         start: f64,
         end: f64,
         step: f64,
-        max_error: na::SVector<f64, 2>,
+        max_error: &na::SVector<f64, 2>,
     ) -> () {
-        let func = |x: na::SVector<f64, 2>, _: f64| {
+        let func = |x: &na::SVector<f64, 2>, _: f64| {
             #[allow(non_snake_case)]
             let A: na::SMatrix<f64, 2, 2> = na::SMatrix::<f64, 2, 2>::new(0., 1., 0., 0.);
             A * x
@@ -214,17 +289,25 @@ mod tests {
         test_integration(
             integrator,
             func,
-            na::SVector::<f64, 2>::new(x0, vel0),
+            &na::SVector::<f64, 2>::new(x0, vel0),
             start,
             end,
             step,
-            na::SVector::<f64, 2>::new(x0 + vel0 * total_time, vel0),
+            &na::SVector::<f64, 2>::new(x0 + vel0 * total_time, vel0),
             max_error,
         );
     }
 
-    ///////////////////////////////////////////////////////////////////////////
-
+    /// Test integrating a constant acceleration system over a time interval [start, end]
+    ///
+    /// # Arguments
+    ///
+    /// * `integrator` - Integrator to use
+    /// * `start` -  Start time for integration
+    /// * `end` - Final time for integration
+    /// * `step` - Step size for integration
+    /// * `max_error` - Maximum allowed error between final result and the expected result
+    ///
     fn test_ca(
         integrator: IntegrationFn<3>,
         x0: f64,
@@ -233,10 +316,10 @@ mod tests {
         start: f64,
         end: f64,
         step: f64,
-        max_error: na::SVector<f64, 3>,
+        max_error: &na::SVector<f64, 3>,
     ) -> () {
         const N: usize = 3;
-        let func = |x: na::SVector<f64, N>, _: f64| {
+        let func = |x: &na::SVector<f64, N>, _: f64| {
             #[allow(non_snake_case)]
             let A: na::SMatrix<f64, N, N> =
                 na::SMatrix::<f64, N, N>::new(0., 1., 0., 0., 0., 1., 0., 0., 0.);
@@ -247,11 +330,11 @@ mod tests {
         test_integration(
             integrator,
             func,
-            na::SVector::<f64, N>::new(x0, vel0, accel0),
+            &na::SVector::<f64, N>::new(x0, vel0, accel0),
             start,
             end,
             step,
-            na::SVector::<f64, N>::new(
+            &na::SVector::<f64, N>::new(
                 x0 + vel0 * total_time + 0.5 * accel0 * total_time * total_time,
                 vel0 + accel0 * total_time,
                 accel0,
@@ -292,7 +375,7 @@ mod tests {
         const END: f64 = 10.;
         const STEP: f64 = 0.01;
         const MAX_ERROR: na::SVector<f64, 2> = na::SVector::<f64, 2>::new(1e-12, 1e-12);
-        test_cv(rk1, X0, VEL0, START, END, STEP, MAX_ERROR);
+        test_cv(rk1, X0, VEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -307,7 +390,7 @@ mod tests {
         const STEP: f64 = 0.01;
 
         const MAX_ERROR: na::SVector<f64, 3> = na::SVector::<f64, 3>::new(1e-1, 1e-12, 1e-12);
-        test_ca(rk1, X0, VEL0, ACCEL0, START, END, STEP, MAX_ERROR);
+        test_ca(rk1, X0, VEL0, ACCEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -342,7 +425,7 @@ mod tests {
         const END: f64 = 10.;
         const STEP: f64 = 0.01;
         const MAX_ERROR: na::SVector<f64, 2> = na::SVector::<f64, 2>::new(1e-12, 1e-12);
-        test_cv(rk2, X0, VEL0, START, END, STEP, MAX_ERROR);
+        test_cv(rk2, X0, VEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -357,7 +440,7 @@ mod tests {
         const STEP: f64 = 0.01;
 
         const MAX_ERROR: na::SVector<f64, 3> = na::SVector::<f64, 3>::new(1e-11, 1e-12, 1e-12);
-        test_ca(rk2, X0, VEL0, ACCEL0, START, END, STEP, MAX_ERROR);
+        test_ca(rk2, X0, VEL0, ACCEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -392,7 +475,7 @@ mod tests {
         const END: f64 = 10.;
         const STEP: f64 = 0.01;
         const MAX_ERROR: na::SVector<f64, 2> = na::SVector::<f64, 2>::new(1e-12, 1e-12);
-        test_cv(rk3, X0, VEL0, START, END, STEP, MAX_ERROR);
+        test_cv(rk3, X0, VEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -407,7 +490,7 @@ mod tests {
         const STEP: f64 = 0.01;
 
         const MAX_ERROR: na::SVector<f64, 3> = na::SVector::<f64, 3>::new(1e-11, 1e-12, 1e-12);
-        test_ca(rk3, X0, VEL0, ACCEL0, START, END, STEP, MAX_ERROR);
+        test_ca(rk3, X0, VEL0, ACCEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -442,7 +525,7 @@ mod tests {
         const END: f64 = 10.;
         const STEP: f64 = 0.01;
         const MAX_ERROR: na::SVector<f64, 2> = na::SVector::<f64, 2>::new(1e-12, 1e-12);
-        test_cv(rk4, X0, VEL0, START, END, STEP, MAX_ERROR);
+        test_cv(rk4, X0, VEL0, START, END, STEP, &MAX_ERROR);
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -457,6 +540,6 @@ mod tests {
         const STEP: f64 = 0.01;
 
         const MAX_ERROR: na::SVector<f64, 3> = na::SVector::<f64, 3>::new(1e-11, 1e-12, 1e-12);
-        test_ca(rk4, X0, VEL0, ACCEL0, START, END, STEP, MAX_ERROR);
+        test_ca(rk4, X0, VEL0, ACCEL0, START, END, STEP, &MAX_ERROR);
     }
 }
