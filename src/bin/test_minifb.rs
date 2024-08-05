066b4f88e4314d973691ec0530deb3420ef0458e
Samir <samirhassen14@gmail.com>
2024-08-05 13:50:59 -0500

    noticed a memory leak when creating a minifb window

---
+++ src/bin/test_minifb.rs
@@ -0,0 +1,15 @@
+use minifb::{Window, WindowOptions};
+
+const W: usize = 800;
+const H: usize = 400;
+
+fn main() -> Result<(), std::io::Error> {
+    let _window = Window::new(
+        &"Here is a title <Esc>=Exit".to_string(),
+        W,
+        H,
+        WindowOptions::default(),
+    );
+
+    Ok(())
+}
