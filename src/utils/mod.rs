d5274cfc500ea5da3b9253af66ed2598b798c787
Samir <samirhassen14@gmail.com>
2024-08-11 13:44:04 -0500

    added def types for utils

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,3 +1,4 @@
-// Not sure how this wpart will go, just making some useful utils
+// Not sure how this part will go, just making some useful utils
+pub mod defs;
 pub mod plot;
 pub mod transforms;
