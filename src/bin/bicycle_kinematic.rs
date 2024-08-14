6f35604f2a79928d8bbe8daa6b68b34e07c1bcea
Samir <samirhassen14@gmail.com>
2024-08-13 23:13:48 -0500

    added new struct types, WindowParams and ChartParams. Functions use these new tpyes instead of individual arguments

--- src/bin/bicycle_kinematic.rs
+++ src/bin/bicycle_kinematic.rs
@@ -1,5 +1,4 @@
 // rust robotics
-// use rust_robotics::models::base;
 use rust_robotics::models::base::System;
 use rust_robotics::models::bicycle_kinematic;
 use rust_robotics::num_methods::runge_kutta;
@@ -9,11 +8,9 @@ use rust_robotics::utils::math;
 use rust_robotics::utils::plot;
 
 // 3rd party or std
+use minifb::{Key, KeyRepeat};
 use nalgebra as na;
 use plotters::prelude::*;
-// use plotters::{chart::ChartState, coord::types::RangedCoordf64};
-use core::f64;
-use minifb::{Key, KeyRepeat};
 use plotters_bitmap::{bitmap_pixel::BGRXPixel, BitMapBackend};
 use std::borrow::{Borrow, BorrowMut};
 use std::collections::VecDeque;
@@ -21,21 +18,12 @@ use std::error::Error;
 use std::time::SystemTime;
 
 // Conversions
-const RAD_TO_DEG: f64 = 180.0 / std::f64::consts::PI;
 const DEG_TO_RAD: f64 = std::f64::consts::PI / 180.0;
 
 // TODO: read from config yml or some type of file for runtime instead of compile time
 // Plot Params
-const WIDTH: usize = 1280;
+const WIDTH: usize = 720;
 const HEIGHT: usize = 720;
-const MARGIN: i32 = 10;
-const BACKGROUND_COLOR: RGBColor = BLACK;
-const LABEL_COLOR: RGBColor = plotters::style::RGBColor(0u8, 255u8, 0u8);
-const LABEL_SIZE: i32 = 30;
-const LABEL_FONT: &str = "sans-serif";
-const LABEL_FONT_SIZE: i32 = 15;
-const X_RANGE: [f64; 2] = [-100.0, 100.0];
-const Y_RANGE: [f64; 2] = [-100.0, 100.0];
 
 // Animation Params
 const SAMPLE_RATE: f64 = 100f64;
@@ -43,7 +31,7 @@ const FPS: f64 = 30f64;
 
 // initial states
 const VEL_INIT: f64 = 1.0;
-const RWA_INIT: f64 = std::f64::consts::FRAC_PI_8;
+const RWA_INIT: f64 = 0.0;
 const VEL_STEP: f64 = 0.1;
 const RWA_STEP: f64 = 1.0 * DEG_TO_RAD;
 const VEL_UPPER_BOUND: f64 = 20.0; // m/s
@@ -51,36 +39,37 @@ const VEL_LOWER_BOUND: f64 = 0.0; // m/s
 const RWA_UPPER_BOUND: f64 = 40.0; // deg
 const RWA_LOWER_BOUND: f64 = -40.0; // deg
 
-// const STATES: usize = 3;
-// const INPUTS: usize = 2;
-
 fn get_window_title(velocity: f64, rwa: f64) -> String {
     format!(
         "velocity={:.1}m/s, rwa={:.1}deg up/down=Adjust vel left/right=Adjust rwa <Esc>=Exit",
         velocity,
-        rwa * RAD_TO_DEG
+        convert::rad_to_deg(rwa)
     )
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut buf = defs::BufferWrapper(vec![0u32; WIDTH * HEIGHT]);
+    let window_params: plot::WindowParams = plot::WindowParams {
+        title: get_window_title(VEL_INIT, RWA_INIT),
+        width: WIDTH,
+        height: HEIGHT,
+    };
 
-    let mut window = plot::create_window(&get_window_title(VEL_INIT, RWA_INIT), WIDTH, HEIGHT);
+    let chart_params: plot::ChartParams = plot::ChartParams {
+        background_color: [0u8, 0u8, 0u8],
+        label_color: [0u8, 255u8, 0u8],
+        margin: 10,
+        label_size: 30,
+        label_font: "sans-serif".to_string(),
+        label_font_size: 15,
+        x_range: [-100.0, 100.0],
+        y_range: [-100.0, 100.0],
+    };
+    let mut buf = defs::BufferWrapper(vec![0u32; window_params.width * window_params.height]);
+
+    let mut window = plot::create_window(&window_params)?;
 
     // TODO: magic numbers
-    let cs = plot::create_2d_chartstate(
-        buf.borrow_mut(),
-        WIDTH as u32,
-        HEIGHT as u32,
-        MARGIN,
-        &BACKGROUND_COLOR,
-        &LABEL_COLOR,
-        LABEL_SIZE,
-        LABEL_FONT,
-        LABEL_FONT_SIZE,
-        X_RANGE,
-        Y_RANGE,
-    );
+    let cs = plot::create_2d_chartstate(buf.borrow_mut(), &window_params, &chart_params);
 
     let mut data: VecDeque<(f64, na::SVector<f64, 3>)> = VecDeque::new();
 
@@ -167,7 +156,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             {
                 let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
                     buf.borrow_mut(),
-                    (WIDTH as u32, HEIGHT as u32),
+                    (window_params.width as u32, window_params.height as u32),
                 )
                 .unwrap()
                 .into_drawing_area();
@@ -184,7 +173,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                     .unwrap();
                 chart
                     .draw_series(data.iter().zip(data.iter().skip(1)).map(
-                        |(&(t0, x0), &(t1, x1))| {
+                        |(&(_t0, x0), &(_t1, x1))| {
                             PathElement::new(vec![(x0[0], x0[1]), (x1[0], x1[1])], &GREEN)
                         },
                     ))
--- src/utils/plot.rs
+++ src/utils/plot.rs
@@ -1,174 +1,107 @@
 #[allow(unused)]
 // rust robotics
 use crate::models::base;
-use crate::num_methods::defs;
-use crate::num_methods::runge_kutta;
-use crate::utils::defs::BufferWrapper;
 
 // 3rd party or std
 use minifb;
-use nalgebra as na;
 use plotters::prelude::*;
 use plotters::{chart::ChartState, coord::types::RangedCoordf64};
 use plotters_bitmap::{bitmap_pixel::BGRXPixel, BitMapBackend};
-use std::borrow::{Borrow, BorrowMut};
-use std::collections::VecDeque;
-use std::time::SystemTime;
+use serde::Deserialize;
+use std::collections::HashMap;
+
+/// Parameters to make a window
+#[derive(Debug, Deserialize)]
+pub struct WindowParams {
+    pub title: String,
+    pub width: usize,
+    pub height: usize,
+}
+
+/// Parameters to make a chart
+#[derive(Debug, Deserialize)]
+pub struct ChartParams {
+    pub background_color: [u8; 3], // RGB
+    pub label_color: [u8; 3],      // RGB
+    pub margin: i32,
+    pub label_size: i32,
+    pub label_font: String,
+    pub label_font_size: i32,
+    pub x_range: [f64; 2],
+    pub y_range: [f64; 2],
+}
 
 #[allow(unused)]
-pub fn create_title(title: &str, keys: &[&str], values: &[f64]) -> String {
+pub fn create_title(title: &str, keyboard_control: &HashMap<minifb::Key, f64>) -> String {
     // "{title}".to_string()
     todo!();
 }
 
 #[allow(unused)]
-pub fn create_window(title: &str, width: usize, height: usize) -> minifb::Window {
-    // TODO: remove unwrap and make the output result because this assumes happy path
-    minifb::Window::new(title, width, height, minifb::WindowOptions::default()).unwrap()
+pub fn create_window(window_params: &WindowParams) -> Result<minifb::Window, minifb::Error> {
+    minifb::Window::new(
+        &window_params.title as &str,
+        window_params.width,
+        window_params.height,
+        minifb::WindowOptions::default(),
+    )
 }
 
 #[allow(unused)]
 pub fn create_2d_chartstate(
     buf: &mut [u8],
-    width: u32,
-    height: u32,
-    margin: i32,
-    background_color: &RGBColor,
-    label_color: &RGBColor,
-    label_size: i32,
-    label_font: &str,
-    label_font_size: i32,
-    x_range: [f64; 2],
-    y_range: [f64; 2],
+    window_params: &WindowParams,
+    chart_params: &ChartParams,
 ) -> ChartState<Cartesian2d<RangedCoordf64, RangedCoordf64>> {
     // TODO: remove unwrap and make the output result because this assumes happy path
+    let background_color = &plotters::style::RGBColor(
+        chart_params.background_color[0],
+        chart_params.background_color[1],
+        chart_params.background_color[2],
+    );
+
+    let label_color = &plotters::style::RGBColor(
+        chart_params.label_color[0],
+        chart_params.label_color[1],
+        chart_params.label_color[2],
+    );
 
     // Expects the buffer to mutable. I think this is to change the pixel values in each buffer
-    let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(buf, (width, height))
-        .unwrap()
-        .into_drawing_area();
+    let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
+        buf,
+        (window_params.width as u32, window_params.height as u32),
+    )
+    .unwrap()
+    .into_drawing_area();
 
     // Establish the background color
     root.fill(background_color).unwrap();
 
     // Chart builds on top of the root drawing backend
     let mut chart = ChartBuilder::on(&root)
-        .margin(margin)
-        .set_left_and_bottom_label_area_size(label_size)
-        .build_cartesian_2d(x_range[0]..x_range[1], y_range[0]..y_range[1])
+        .margin(chart_params.margin)
+        .set_left_and_bottom_label_area_size(chart_params.label_size)
+        .build_cartesian_2d(
+            chart_params.x_range[0]..chart_params.x_range[1],
+            chart_params.y_range[0]..chart_params.y_range[1],
+        )
         .unwrap();
 
     // Establish the chart styles
     chart
         .configure_mesh()
         .label_style(
-            (label_font, label_font_size)
+            (
+                &chart_params.label_font as &str,
+                chart_params.label_font_size,
+            )
                 .into_font()
-                .color(&label_color),
+                .color(label_color),
         )
-        .axis_style(&label_color)
+        .axis_style(label_color)
         .draw()
         .unwrap();
 
     // Converts ChartContext to ChartState to use for later
     chart.into_chart_state()
 }
-
-// #[allow(unused)]
-// TODO: this function is doing a lot, break it up, possibly make it more generic with types,
-// need num-traits if I want to go that route most likely
-// TODO: OMG SO MANY INPUTS
-// TODO: Take a hard look at this horrendous function
-// pub fn update_2d_chartstate<const N: usize, const M: usize>(
-//     mut window: minifb::Window,
-//     mut buf: BufferWrapper,
-//     mut chart: ChartState<Cartesian2d<RangedCoordf64, RangedCoordf64>>,
-//     integrator: impl defs::IntegrationFn<f64, N>,
-//     model: impl base::System<f64, N, M>,
-//     mut data: VecDeque<(f64, na::SVector<f64, N>)>,
-//     mut current_state: na::SVector<f64, N>,
-//     mut current_input: na::SVector<f64, M>,
-//     samples_per_sec: f64,
-//     frames_per_sec: f64,
-//     width: u32,
-//     height: u32,
-//     margin: i32,
-//     label_size: i32,
-//     label_font: &str,
-//     label_font_size: i32,
-//     label_color: &RGBColor,
-//     x_range: [f64; 2],
-//     y_range: [f64; 2],
-// ) -> () {
-//     let start_time = SystemTime::now();
-//     let mut last_flushed = 0.;
-//     let sample_step: f64 = 1.0 / samples_per_sec;
-//     let frame_step: f64 = 1.0 / frames_per_sec;
-
-//     while window.is_open() && !window.is_key_down(minifb::Key::Escape) {
-//         let epoch = SystemTime::now()
-//             .duration_since(start_time)
-//             .unwrap()
-//             .as_secs_f64();
-
-//         if let Some((ts, _)) = data.back() {
-//             if epoch - ts < 1.0 / samples_per_sec {
-//                 std::thread::sleep(std::time::Duration::from_secs_f64(epoch - ts));
-//                 continue;
-//             }
-//             let mut ts = *ts;
-//             while ts < epoch {
-//                 current_state = model.propagate(
-//                     &current_state,
-//                     &current_input,
-//                     epoch,
-//                     sample_step,
-//                     &integrator,
-//                 );
-//                 ts += sample_step;
-
-//                 data.push_back((ts, current_state));
-//             }
-//         }
-
-//         // add data
-//         // TODO: disingenuous cuz how will sample_step be the time step if using system time?
-//         // might go away from system time
-//         current_state = model.propagate(
-//             &current_state,
-//             &current_input,
-//             epoch,
-//             sample_step,
-//             &integrator,
-//         );
-
-//         if epoch - last_flushed > frame_step {
-//             let root = BitMapBackend::<RGBPixel>::with_buffer_and_format(
-//                 buf.borrow_mut(),
-//                 (width, height),
-//             )
-//             .unwrap()
-//             .into_drawing_area();
-//             root.fill(&BLACK).unwrap();
-
-//             let mut cs = chart.clone().restore(&root);
-//             cs.plotting_area().fill(&BLACK).unwrap();
-
-//             cs.configure_mesh()
-//                 .bold_line_style(&GREEN.mix(0.2))
-//                 .light_line_style(&TRANSPARENT)
-//                 .draw()
-//                 .unwrap();
-//             cs.draw_series(
-//                 data.iter()
-//                     .zip(data.iter().skip(1))
-//                     .map(|(&(t0, x0), &(t1, x1))| {
-//                         PathElement::new(vec![(x0[0], x0[1]), (x1[0], x1[1])], &GREEN)
-//                     }),
-//             )
-//             .unwrap();
-//         }
-//         window.update_with_buffer(buf.borrow(), width as usize, height as usize);
-//     }
-//     todo!();
-// }
