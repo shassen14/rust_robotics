075b12cf7fb3b7401b0caaae9c68ce702408adef
Samir <samirhassen14@gmail.com>
2024-08-16 11:32:47 -0500

    delete code and replaced it with calc_rectangle

--- src/utils/plot.rs
+++ src/utils/plot.rs
@@ -129,60 +129,13 @@ pub fn create_2d_chartstate(
 }
 
 #[allow(unused)]
-pub fn draw_2d_polygon(
-    buf: &mut [u8],
-    window_params: &WindowParams,
+pub fn calculate_rectangle<T>(
+    start_point: &[T; 2],
+    length_forward: T,
+    length_back: T,
+    width: T,
+    heading_angle: T,
     chart_params: &ChartParams,
-) -> ChartState<Cartesian2d<RangedCoordf64, RangedCoordf64>> {
-    // TODO: remove unwrap and make the output result because this assumes happy path
-    let background_color = &plotters::style::RGBColor(
-        chart_params.background_color[0],
-        chart_params.background_color[1],
-        chart_params.background_color[2],
-    );
-
-    let label_color = &plotters::style::RGBColor(
-        chart_params.label_color[0],
-        chart_params.label_color[1],
-        chart_params.label_color[2],
-    );
-
-    // Expects the buffer to mutable. I think this is to change the pixel values in each buffer
-    let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
-        buf,
-        (window_params.width as u32, window_params.height as u32),
-    )
-    .unwrap()
-    .into_drawing_area();
-
-    // Establish the background color
-    root.fill(background_color).unwrap();
-
-    // Chart builds on top of the root drawing backend
-    let mut chart = ChartBuilder::on(&root)
-        .margin(chart_params.margin)
-        .set_left_and_bottom_label_area_size(chart_params.label_size)
-        .build_cartesian_2d(
-            chart_params.x_range[0]..chart_params.x_range[1],
-            chart_params.y_range[0]..chart_params.y_range[1],
-        )
-        .unwrap();
-
-    // Establish the chart styles
-    chart
-        .configure_mesh()
-        .label_style(
-            (
-                &chart_params.label_font as &str,
-                chart_params.label_font_size,
-            )
-                .into_font()
-                .color(label_color),
-        )
-        .axis_style(label_color)
-        .draw()
-        .unwrap();
-
-    // Converts ChartContext to ChartState to use for later
-    chart.into_chart_state()
+) -> PathElement<(T, T)> {
+    todo!();
 }
