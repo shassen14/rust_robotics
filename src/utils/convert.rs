1d561b7a85d89bb908faf0912c2d6c80db833c82
Samir <samirhassen14@gmail.com>
2024-08-16 01:39:35 -0500

    convert tests

--- src/utils/convert.rs
+++ src/utils/convert.rs
@@ -25,3 +25,41 @@ where
 {
     (std::f64::consts::PI / 180f64) * angle
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+
+    #[test]
+    fn test_deg_to_rad_f64() {
+        let angle_deg = [0., 45., 90., 135., 180.];
+        let angle_rad = [
+            0.,
+            std::f64::consts::FRAC_PI_4,
+            std::f64::consts::FRAC_PI_2,
+            3. * std::f64::consts::FRAC_PI_4,
+            std::f64::consts::PI,
+        ];
+
+        for i in 0..5 {
+            assert_relative_eq!(deg_to_rad(angle_deg[i]), angle_rad[i], epsilon = 1e-12);
+        }
+    }
+
+    #[test]
+    fn test_rad_to_deg_f64() {
+        let angle_deg = [0., 45., 90., 135., 180.];
+        let angle_rad = [
+            0.,
+            std::f64::consts::FRAC_PI_4,
+            std::f64::consts::FRAC_PI_2,
+            3. * std::f64::consts::FRAC_PI_4,
+            std::f64::consts::PI,
+        ];
+
+        for i in 0..5 {
+            assert_relative_eq!(rad_to_deg(angle_rad[i]), angle_deg[i], epsilon = 1e-12);
+        }
+    }
+}
