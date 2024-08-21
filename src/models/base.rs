6eba805803907b065077b0c47cab2f23e4af6954
Samir <samirhassen14@gmail.com>
2024-08-21 15:13:32 -0500

    added jacobian calculations to the bicycle kinematic model for both states and input

--- src/models/base.rs
+++ src/models/base.rs
@@ -35,6 +35,41 @@ pub trait System<T, const N: usize, const M: usize> {
         integrator(&func, x, t, t + dt)
     }
 
+    // TODO: is this needed if propagate exists? May want to delete
+    /// Propagates one time step given the state space model x_dot = Ax + Bu
+    ///
+    /// # Arguments
+    ///
+    /// * `self` - Model's parameters, functions, and values
+    /// * `x` - System's current state
+    /// * `u` - System's current control input
+    /// * `t` - Current timestamp
+    /// * `dt` - Timestep
+    /// * `integrator` - Integration function to integrate over one timestep
+    /// * Returns Final State x(t+ dt)
+    ///
+    fn propagate_linear(
+        &self,
+        x: &na::SVector<T, N>,
+        u: &na::SVector<T, M>,
+        t: T,
+        dt: T,
+        integrator: impl defs::IntegrationFn<T, N>,
+    ) -> na::SVector<T, N>
+    where
+        T: std::ops::Add<Output = T> + Copy,
+        na::SMatrix<T, N, N>: std::ops::Mul<na::SVector<T, N>, Output = na::SVector<T, N>>,
+        na::SMatrix<T, N, M>: std::ops::Mul<na::SVector<T, M>, Output = na::SVector<T, N>>,
+        na::SVector<T, N>: std::ops::Add<Output = na::SVector<T, N>>,
+    {
+        let func = |func_x: &na::SVector<T, N>, func_t: T| -> na::SVector<T, N> {
+            let linear_model = self.get_jacobian(func_x, u, func_t);
+            // x_dot = Ax + Bu
+            linear_model.0 * *func_x + linear_model.1 * *u
+        };
+        integrator(&func, x, t, t + dt)
+    }
+
     /// Derivative function x_dot = f(x, u, t) which is a function of
     /// the system's current state, control input, and time
     ///
@@ -64,7 +99,7 @@ pub trait System<T, const N: usize, const M: usize> {
     /// * Returns a pair of matrices where the first is the partial derivative of f(x, u, t)
     /// with respect to each state (x), and the second is the partial derivative of f(x, u, t)
     /// with respect to each control input (u). [(NxN), (NxM)] since there are n states and
-    /// M inputs.
+    /// M inputs. These are the A and B matrices for linear systems
     ///
     fn get_jacobian(
         &self,
--- src/models/bicycle_kinematic.rs
+++ src/models/bicycle_kinematic.rs
@@ -63,30 +63,125 @@ impl Model {
     /// * Arguments
     ///
     /// * `self` - Model's parameters
-    /// * `steer_angle` - the road wheel angle the wheels are facing relative the the vehicle direction
+    /// * `road_wheel_angle` - the road wheel angle the wheels are facing relative the the vehicle direction
     /// * Returns the length from the rear wheels (middle) to the center of gravity
-    fn calculate_sideslip(&self, steer_angle: f64) -> f64 {
-        f64::atan(self.length_rear * f64::tan(steer_angle) / self.calculate_length_total())
+    ///
+    fn calculate_sideslip(&self, road_wheel_angle: f64) -> f64 {
+        // beta = atan(l_r * tan(rwa) / wheel_base)
+        f64::atan(self.length_rear * f64::tan(road_wheel_angle) / self.calculate_length_total())
     }
 
-    /// TODO: Derivative of the nonlinear model is slightly difficult. Need to figure out how to take partial derivatives
-    /// with different frames easily. Might add an enum for it?
+    // TODO: calculate a and calculate b seem to be right? Confirmed with online calculators
+    // Simulation seem to be way off unfortunately. Need to reevaluate this
     #[allow(unused)]
-    fn calculate_f(&self, steer_angle: f64) -> na::SMatrix<f64, 3, 3> {
+    fn calculate_a(
+        &self,
+        x: &na::SVector<f64, 3>,
+        u: &na::SVector<f64, 2>,
+    ) -> na::SMatrix<f64, 3, 3> {
+        let sideslip: f64 = self.calculate_sideslip(u[1]);
+
+        // df0/ dx0, df0/ dx1, df0 / dx2
+        // df1/ dx0, df1/ dx1, df1 / dx2
+        // df2/ dx0, df2/ dx1, df2 / dx2
+        // where
+        // f0: x_dot = vel_x * cos(yaw + sideslip)
+        // f1: y_dot = vel_x * sin(yaw + sideslip)
+        // f2: yaw_dot = vel_x * tan(road_wheel_angle) * cos(sideslip) / length_total
+
+        let df0x0 = 0f64;
+        let df0x1 = 0f64;
+        let df0x2 = -u[0] * f64::sin(x[2] + sideslip);
+
+        let df1x0 = 0f64;
+        let df1x1 = 0f64;
+        let df1x2 = u[0] * f64::cos(x[2] + sideslip);
+
+        let df2x0 = 0f64;
+        let df2x1 = 0f64;
+        let df2x2 = 0f64;
+
+        // inputs by columns
         na::SMatrix::<f64, 3, 3>::from_array_storage(na::ArrayStorage([
-            [0., 0., 0.],
-            [1., 0., 0.],
-            [0., 1., 0.],
-        ]));
-        // let x1
-        todo!();
+            [df0x0, df1x0, df2x0],
+            [df0x1, df1x1, df2x1],
+            [df0x2, df1x2, df2x2],
+        ]))
+    }
+
+    #[allow(unused)]
+    fn calculate_b(
+        &self,
+        x: &na::SVector<f64, 3>,
+        u: &na::SVector<f64, 2>,
+    ) -> na::SMatrix<f64, 3, 2> {
+        let length_total = self.calculate_length_total();
+        let sideslip = self.calculate_sideslip(u[1]);
+        // df0/ du0, df0/ du1,
+        // df1/ du0, df1/ du1,
+        // df2/ du0, df2/ du1,
+        // where
+        // f0: x_dot = vel_x * cos(yaw + sideslip)
+        // f1: y_dot = vel_x * sin(yaw + sideslip)
+        // f2: yaw_dot = vel_x * tan(road_wheel_angle) * cos(sideslip) / length_total
+        let df0u0 = f64::cos(x[2] + sideslip);
+        let df1u0 = f64::sin(x[2] + sideslip);
+
+        let df2u0_denom = length_total
+            * f64::sqrt(
+                f64::powi(self.length_rear, 2) * f64::powi(f64::tan(u[1]), 2)
+                    / f64::powi(length_total, 2)
+                    + 1.,
+            );
+
+        // let df2u0 = f64::tan(u[1]) * f64::cos(sideslip) / length_total;
+        let df2u0 = f64::tan(u[1]) / df2u0_denom;
+
+        // derivative of sideslip wrt road wheel angle for the first two terms.
+        // coefficient is l_r * t * sec^2(u[1])
+        let dfu1_coeff = self.length_rear * length_total / (f64::cos(u[1]) * f64::cos(u[1]));
+
+        // derivative of sideslip wrt road wheel angle for the first two terms.
+        // denominator = l_r^2 * tan^2(u[1]) + l_t^2
+        let dfu1_denom = (self.length_rear * self.length_rear) * (f64::tan(u[1]) * f64::tan(u[1]))
+            + (length_total * length_total);
+
+        // // numerator for last term
+        // let num = length_total * u[0] / (f64::cos(u[1]) * f64::cos(u[1]));
+        // // denominator for last term
+        // let den = ((self.length_rear * self.length_rear) * (f64::tan(u[1]) * f64::tan(u[1]))
+        //     + (length_total * length_total))
+        //     * f64::sqrt(
+        //         (self.length_rear * self.length_rear) * (f64::tan(u[1]) * f64::tan(u[1]))
+        //             / (length_total * length_total)
+        //             + 1.0,
+        //     );
+
+        // numerator for last term
+        let df2u1_num = f64::powf(length_total, 3f64) * u[0] / (f64::cos(u[1]) * f64::cos(u[1]));
+        // denominator for last term
+        let df2u1_denom = f64::powf(
+            (self.length_rear * self.length_rear) * (f64::tan(u[1]) * f64::tan(u[1]))
+                + (length_total * length_total),
+            1.5f64,
+        ) * length_total.abs();
+
+        let df0u1 = -u[0] * dfu1_coeff * f64::sin(x[2] + sideslip) / dfu1_denom;
+        let df1u1 = u[0] * dfu1_coeff * f64::cos(x[2] + sideslip) / dfu1_denom;
+        let df2u1 = df2u1_num / df2u1_denom;
+
+        // input by column
+        na::SMatrix::<f64, 3, 2>::from_array_storage(na::ArrayStorage([
+            [df0u0, df1u0, df2u0],
+            [df0u1, df1u1, df2u1],
+        ]))
     }
 }
 
 /// Bicycle Kinematic Model with 3 states and 2 inputs
 ///
 /// x_dot = [pos_x_dot, pos_y_dot, heading_dot], global frame
-/// u = [vel_x, steering_angle], body frame
+/// u = [vel_x, road_wheel_angle], body frame
 impl base::System<f64, 3, 2> for Model {
     /// Derivative function x_dot = f(x, u, t) which is a function of
     /// the system's current state, control input, and time
@@ -109,7 +204,7 @@ impl base::System<f64, 3, 2> for Model {
 
         // x_dot = vel_x * cos(yaw + sideslip)
         // y_dot = vel_x * sin(yaw + sideslip)
-        // yaw_dot = vel_x * tan(steer_angle) * cos(sideslip) / length_total
+        // yaw_dot = vel_x * tan(road_wheel_angle) * cos(sideslip) / length_total
         let pos_x_dot: f64 = u[0] * f64::cos(x[2] + sideslip);
         let pos_y_dot: f64 = u[0] * f64::sin(x[2] + sideslip);
         let yaw_dot: f64 =
@@ -132,15 +227,14 @@ impl base::System<f64, 3, 2> for Model {
     ///
     fn get_jacobian(
         &self,
-        _x: &nalgebra::SVector<f64, 3>,
-        u: &nalgebra::SVector<f64, 2>,
+        x: &na::SVector<f64, 3>,
+        u: &na::SVector<f64, 2>,
         _t: f64,
     ) -> (na::SMatrix<f64, 3, 3>, na::SMatrix<f64, 3, 2>) {
         (
-            Model::calculate_f(&Model::new(self.length_front, self.length_rear), u[1]),
-            na::SMatrix::<f64, 2, 2>::zeros(),
-        );
-        todo!();
+            Model::calculate_a(&self, x, u),
+            Model::calculate_b(&self, x, u),
+        )
     }
 
     /// Read a toml file to change the model's parameters if any
@@ -163,10 +257,11 @@ impl base::System<f64, 3, 2> for Model {
 ///////////////////////////////////////////////////////////////////////////////
 #[cfg(test)]
 mod tests {
-    use core::f64;
 
     use super::*;
     use crate::num_methods::runge_kutta;
+    use approx::assert_relative_eq;
+    use base::System;
 
     #[test]
     fn bicycle_kinem_prop_straight() {
@@ -228,7 +323,7 @@ mod tests {
         let yaw0: f64 = 0.;
 
         let vel0: f64 = 5.;
-        let steer0: f64 = f64::consts::FRAC_PI_8;
+        let steer0: f64 = std::f64::consts::FRAC_PI_8;
 
         let start: f64 = 0.;
         let end: f64 = 10.;
@@ -261,5 +356,33 @@ mod tests {
             assert!(result[i] > min_result);
             assert!(result[i] < max_result);
         }
+    }
+
+    #[test]
+    fn bicycle_kinem_jacobian() {
+        // TODO: need to check the 2nd jacobian for B matrix
+
+        let lf: f64 = 1.0;
+        let lr: f64 = 1.0;
+        let veh: Model = Model::new(lf, lr);
+
+        let x0: f64 = 0.;
+        let y0: f64 = x0;
+        let yaw0: f64 = 0.;
+
+        let vel0: f64 = 4.;
+        let steer0: f64 = std::f64::consts::FRAC_PI_8 * 1.0;
+
+        let state0: na::SVector<f64, 3> = na::SVector::<f64, 3>::new(x0, y0, yaw0);
+        let input0: na::SVector<f64, 2> = na::SVector::<f64, 2>::new(vel0, steer0);
+
+        let jacobian = veh.get_jacobian(&state0, &input0, 0.);
+
+        // the magnitude of x_dot and y_dot combined should equal to forward velocity
+        // this model assumes lateral velocity ~ 0
+        let calc1 = f64::sqrt(
+            jacobian.0[(0, 2)] * jacobian.0[(0, 2)] + jacobian.0[(1, 2)] * jacobian.0[(1, 2)],
+        );
+        assert_relative_eq!(calc1, vel0, epsilon = 1e-12);
     }
 }
