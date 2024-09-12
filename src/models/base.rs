429b01e3f6f38103b4a9de6c01d1d78b2355231f
shassen14 <samirhassen14@gmail.com>
2024-09-12 15:26:08 -0500

    removed propagate_linear function from base model

--- src/models/base.rs
+++ src/models/base.rs
@@ -51,31 +51,31 @@ pub trait System<T, const N: usize, const M: usize> {
     /// * `dt` - Timestep
     /// * `integrator` - Integration function to integrate over one timestep
     /// * Returns Final State x(t+ dt)
-    fn propagate_linear(
-        &self,
-        x: &na::SVector<T, N>,
-        u: &na::SVector<T, M>,
-        t: T,
-        dt: T,
-        integrator: impl defs::IntegrationFn<T, N>,
-    ) -> na::SVector<T, N>
-    where
-        T: std::ops::Add<Output = T> + Copy,
-        na::SMatrix<T, N, N>: std::ops::Mul<na::SVector<T, N>, Output = na::SVector<T, N>>,
-        na::SMatrix<T, N, M>: std::ops::Mul<na::SVector<T, M>, Output = na::SVector<T, N>>,
-        na::SVector<T, N>: std::ops::Add<Output = na::SVector<T, N>>,
-    {
-        let func = |func_x: &na::SVector<T, N>, func_t: T| -> na::SVector<T, N> {
-            let linear_model = self.calculate_jacobian(func_x, u, func_t);
-            // x_dot = Ax + Bu
-            // TODO: the correct formula is something dx_dot = A* dx + B du
-            // since the jacobian is derivative relative to the state and inputs.
-            // Would keeping track of previous states help?
-            // Integrating this would obtain dx and then add that to the current x?
-            linear_model.0 * *func_x + linear_model.1 * *u
-        };
-        integrator(&func, x, t, t + dt)
-    }
+    // fn propagate_linear(
+    //     &self,
+    //     x: &na::SVector<T, N>,
+    //     u: &na::SVector<T, M>,
+    //     t: T,
+    //     dt: T,
+    //     integrator: impl defs::IntegrationFn<T, N>,
+    // ) -> na::SVector<T, N>
+    // where
+    //     T: std::ops::Add<Output = T> + Copy,
+    //     na::SMatrix<T, N, N>: std::ops::Mul<na::SVector<T, N>, Output = na::SVector<T, N>>,
+    //     na::SMatrix<T, N, M>: std::ops::Mul<na::SVector<T, M>, Output = na::SVector<T, N>>,
+    //     na::SVector<T, N>: std::ops::Add<Output = na::SVector<T, N>>,
+    // {
+    //     let func = |func_x: &na::SVector<T, N>, func_t: T| -> na::SVector<T, N> {
+    //         let linear_model = self.calculate_jacobian(func_x, u, func_t);
+    //         // x_dot = Ax + Bu
+    //         // TODO: the correct formula is something dx_dot = A* dx + B du
+    //         // since the jacobian is derivative relative to the state and inputs.
+    //         // Would keeping track of previous states help?
+    //         // Integrating this would obtain dx and then add that to the current x?
+    //         linear_model.0 * *func_x + linear_model.1 * *u
+    //     };
+    //     integrator(&func, x, t, t + dt)
+    // }
 
     /// Derivative function x_dot = f(x, u, t) which is a function of
     /// the system's current state, control input, and time
