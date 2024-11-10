480539c63f149381a879955aee15c490e670db58
Samir <samirhassen14@gmail.com>
2024-11-09 21:38:46 -0600

    made dijkstra less efficient for animation purposes

--- src/path_planning/dijkstra.rs
+++ src/path_planning/dijkstra.rs
@@ -6,7 +6,7 @@ use std::hash::Hash;
  * N: Node type
  * C: Cost type
  */
-#[derive(Clone, Copy, std::fmt::Debug)]
+#[derive(Clone, Copy)]
 struct DijkstraItem<N, C> {
     pub node: N,
     pub parent: Option<N>,
@@ -18,8 +18,8 @@ fn calculate_final_path<N, C>(
     closed_set: &FxHashMap<N, DijkstraItem<N, C>>,
 ) -> Vec<N>
 where
-    N: Hash + Eq + Copy + std::fmt::Debug,
-    C: Zero + Ord + Copy + std::fmt::Debug,
+    N: Hash + Eq + Copy,
+    C: Zero + Ord + Copy,
 {
     let mut path: Vec<N> = vec![goal_item.node];
 
@@ -44,7 +44,6 @@ where
 {
     // Default is none and false
     let mut path: Option<Vec<N>> = Option::default();
-    let mut is_path_found: bool = bool::default();
 
     let start_item = DijkstraItem::<N, C> {
         node: start.clone(),
@@ -64,60 +63,87 @@ where
     open_set.insert(start.clone(), start_item);
 
     while !open_set.is_empty() {
-        // should be safe to unwrap because of the while loop conditional statement
-        let min_cost_index = *open_set
-            .iter()
-            .min_by(|a, b| a.1.cost.cmp(&b.1.cost))
-            .unwrap()
-            .0;
-
-        let current_item = *open_set.get(&min_cost_index).unwrap();
-
-        if current_item.node == *goal {
-            println!("Found goal");
-            is_path_found = true;
-            goal_item.parent = current_item.parent;
-            goal_item.cost = current_item.cost;
-            closed_set.insert(*goal, goal_item);
+        if plan_once(&mut open_set, &mut closed_set, children_fn, &mut goal_item).1 {
             break;
-        }
-
-        open_set.remove(&min_cost_index);
-        closed_set.insert(min_cost_index.clone(), current_item.clone());
-
-        // Determine the valid children using the function given
-        let children = children_fn(&current_item.node);
-
-        for (child, child_cost) in children {
-            // if the child is already in the set, no need to check because the cost
-            // would be more than what is already in the list
-            if closed_set.contains_key(&child) {
-                continue;
-            }
-
-            let child_item = DijkstraItem::<N, C> {
-                node: child,
-                parent: Some(current_item.node),
-                cost: current_item.cost + child_cost,
-            };
-
-            // if child has never been seen before, add to the open list
-            // if seen and the child has less cost than previously, change the node to reflect
-            // a cheaper path
-            if !open_set.contains_key(&child) {
-                open_set.insert(child, child_item);
-            } else {
-                if child_item.cost < open_set.get(&child).unwrap().cost {
-                    open_set.insert(child, child_item);
-                }
-            }
         }
     }
 
-    if is_path_found {
-        println!("hello calculating path");
+    if closed_set.contains_key(goal) {
         path = Some(calculate_final_path(&goal_item, &closed_set));
     }
 
     path
+}
+
+// TODO: Lot of copying. Reduce the amount
+/// This is intended for graphing purposes. It's very inefficient, but good to show what nodes are being
+/// populated at each time step.
+fn plan_once<N, C, FN, IN>(
+    open_set: &mut FxHashMap<N, DijkstraItem<N, C>>,
+    closed_set: &mut FxHashMap<N, DijkstraItem<N, C>>,
+    children_fn: &mut FN,
+    goal_item: &mut DijkstraItem<N, C>,
+) -> (Option<Vec<N>>, bool)
+where
+    N: Hash + Eq + Copy,
+    C: Zero + Ord + Copy,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C)>,
+{
+    // should be safe to unwrap because of the while loop conditional statement
+
+    let min_cost_index = *open_set
+        .iter()
+        .min_by(|a, b| a.1.cost.cmp(&b.1.cost))
+        .unwrap()
+        .0;
+
+    let current_item = *open_set.get(&min_cost_index).unwrap();
+
+    if current_item.node == goal_item.node {
+        println!("Found goal");
+        goal_item.parent = current_item.parent;
+        goal_item.cost = current_item.cost;
+        closed_set.insert(goal_item.node, *goal_item);
+        return (
+            Some(closed_set.clone().keys().into_iter().map(|i| *i).collect()),
+            true,
+        );
+    }
+
+    open_set.remove(&min_cost_index);
+    closed_set.insert(min_cost_index.clone(), current_item.clone());
+
+    // Determine the valid children using the function given
+    let children = children_fn(&current_item.node);
+
+    for (child, child_cost) in children {
+        // if the child is already in the set, no need to check because the cost
+        // would be more than what is already in the list
+        if closed_set.contains_key(&child) {
+            continue;
+        }
+
+        let child_item = DijkstraItem::<N, C> {
+            node: child,
+            parent: Some(current_item.node),
+            cost: current_item.cost + child_cost,
+        };
+
+        // if child has never been seen before, add to the open list
+        // if seen and the child has less cost than previously, change the node to reflect
+        // a cheaper path
+        if !open_set.contains_key(&child) {
+            open_set.insert(child, child_item);
+        } else {
+            if child_item.cost < open_set.get(&child).unwrap().cost {
+                open_set.insert(child, child_item);
+            }
+        }
+    }
+
+    (
+        Some(closed_set.clone().keys().into_iter().map(|i| *i).collect()),
+        false,
+    )
 }
