2ed51e7c54ac0f21ba8fc983534b223186420073
Samir <samirhassen14@gmail.com>
2024-11-11 16:23:15 -0600

    generating maps with circle objects via grid like

--- src/utils/map_generator.rs
+++ src/utils/map_generator.rs
@@ -27,11 +27,13 @@ where
         let x_length: usize = ((x_range[1] - x_range[0]) / resolution)
             .round()
             .to_usize()
-            .expect("Couldn't convert Type U to isize");
+            .expect("Couldn't convert Type U to usize")
+            + 1usize;
         let y_length: usize = ((y_range[1] - y_range[0]) / resolution)
             .round()
             .to_usize()
-            .expect("Couldn't convert Type U to isize");
+            .expect("Couldn't convert Type U to usize")
+            + 1usize;
 
         // TODO: check if x_length or y_length
         let mut obs_map: Vec<Vec<T>> = vec![vec![Zero::zero(); x_length]; y_length];
@@ -39,7 +41,6 @@ where
 
         Self::process_shapes(&mut obs_map, &new_obstacles);
 
-        println!("{:?}", obs_map);
         GridMap2D::<T, U> {
             map: obs_map,
             x_range: *x_range,
@@ -53,8 +54,8 @@ where
         x_range: &[U; 2],
         y_range: &[U; 2],
         resolution: U,
-    ) -> Vec<(geometry::Shape2D<isize>, T)> {
-        let mut obs_index: Vec<(geometry::Shape2D<isize>, T)> = Vec::with_capacity(obstacles.len());
+    ) -> Vec<(geometry::Shape2D<usize>, T)> {
+        let mut obs_index: Vec<(geometry::Shape2D<usize>, T)> = Vec::with_capacity(obstacles.len());
 
         for (shape, cost) in obstacles {
             match shape {
@@ -64,17 +65,17 @@ where
                             (
                                 ((circle.center.0 - x_range[0]) / resolution)
                                     .round()
-                                    .to_isize()
-                                    .expect("cannot convert f64 to isize"),
+                                    .to_usize()
+                                    .expect("cannot convert f64 to usize"),
                                 ((circle.center.1 - y_range[0]) / resolution)
                                     .round()
-                                    .to_isize()
-                                    .expect("cannot convert f64 to isize"),
+                                    .to_usize()
+                                    .expect("cannot convert f64 to usize"),
                             ),
                             (circle.radius / resolution)
                                 .round()
-                                .to_isize()
-                                .expect("cannot convert f64 to isize"),
+                                .to_usize()
+                                .expect("cannot convert f64 to usize"),
                         )),
                         *cost,
                     ));
@@ -86,28 +87,37 @@ where
         obs_index
     }
 
-    fn process_shapes(map: &mut Vec<Vec<T>>, obstacles: &Vec<(geometry::Shape2D<isize>, T)>) -> () {
+    fn process_shapes(map: &mut Vec<Vec<T>>, obstacles: &Vec<(geometry::Shape2D<usize>, T)>) -> () {
         // TODO: very slow, time complexity is MNO where M is number of obstacles,
         // N is rows, O is columns
         for (shape, cost) in obstacles {
             match shape {
-                geometry::Shape2D::<isize>::Circle(circle) => {
+                geometry::Shape2D::<usize>::Circle(circle) => {
                     // row or y
                     for (y_index, row) in map.clone().iter().enumerate() {
                         // columng or x
-                        for (x_index, _col) in row.iter().enumerate() {
-                            let x_dif = (circle.center.0 - x_index as isize) as f64;
-                            let y_dif = (circle.center.1 - y_index as isize) as f64;
+                        for (x_index, _value) in row.iter().enumerate() {
+                            let x_dif = if circle.center.0 > x_index {
+                                (circle.center.0 - x_index) as f64
+                            } else {
+                                (x_index - circle.center.0) as f64
+                            };
+                            let y_dif = if circle.center.1 > y_index {
+                                (circle.center.1 - y_index) as f64
+                            } else {
+                                (y_index - circle.center.1) as f64
+                            };
                             let distance =
-                                f64::sqrt(x_dif * x_dif + y_dif * y_dif).round() as isize;
+                                f64::sqrt(x_dif * x_dif + y_dif * y_dif).round() as usize;
 
-                            if distance < circle.radius {
+                            // TODO: fill or just shell modes?
+                            if distance <= circle.radius {
                                 map[y_index][x_index] = *cost;
                             }
                         }
                     }
                 }
-                geometry::Shape2D::<isize>::Polygon(polygon) => {}
+                geometry::Shape2D::<usize>::Polygon(polygon) => {}
             }
         }
     }
@@ -121,13 +131,13 @@ mod tests {
     #[test]
     fn test_gridmap() {
         let obstacles = vec![(
-            geometry::Shape2D::<f64>::Circle(CircleS::new((0.0, 0.0), 1.5)),
+            geometry::Shape2D::<f64>::Circle(CircleS::new((0.0, 0.0), 1.0)),
             1.0,
         )];
 
-        let x_range: [f64; 2] = [-5.0, 5.0];
-        let y_range: [f64; 2] = [-5.0, 5.0];
-        let resolution: f64 = 1.0;
+        let x_range: [f64; 2] = [-1.0, 1.0];
+        let y_range: [f64; 2] = [-1.0, 1.0];
+        let resolution: f64 = 0.1;
         let map = GridMap2D::<f64, f64>::new(&obstacles, &x_range, &y_range, resolution);
 
         assert!(true);
