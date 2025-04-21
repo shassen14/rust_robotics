cb91b992920bf2c15410e4c316f86f3515de8a6f
Samir <samirhassen14@gmail.com>
2025-04-21 11:18:38 -0500

    Remove linear_propagate. Don't think it'll work

--- src/models/base.rs
+++ src/models/base.rs
@@ -3,7 +3,7 @@ use nalgebra as na;
 
 // TODO: Add a `write`` function to write to a toml for the system parameters
 // This would be helpful with system identification
-// TODO: Add `estimate model` function to east imate the parameters given the
+// TODO: Add `estimate model` function to estimate the parameters given the
 // current state and state_dot
 
 /// System is a public interface that all models should implement to
@@ -38,44 +38,6 @@ pub trait System<T, const N: usize, const M: usize> {
         };
         integrator(&func, x, t, t + dt)
     }
-
-    // TODO: is this needed if propagate exists? May want to delete
-    /// Propagates one time step given the state space model x_dot = Ax + Bu
-    ///
-    /// # Arguments
-    ///
-    /// * `self` - Model's parameters, functions, and values
-    /// * `x` - System's current state
-    /// * `u` - System's current control input
-    /// * `t` - Current timestamp
-    /// * `dt` - Timestep
-    /// * `integrator` - Integration function to integrate over one timestep
-    /// * Returns Final State x(t+ dt)
-    // fn propagate_linear(
-    //     &self,
-    //     x: &na::SVector<T, N>,
-    //     u: &na::SVector<T, M>,
-    //     t: T,
-    //     dt: T,
-    //     integrator: impl defs::IntegrationFn<T, N>,
-    // ) -> na::SVector<T, N>
-    // where
-    //     T: std::ops::Add<Output = T> + Copy,
-    //     na::SMatrix<T, N, N>: std::ops::Mul<na::SVector<T, N>, Output = na::SVector<T, N>>,
-    //     na::SMatrix<T, N, M>: std::ops::Mul<na::SVector<T, M>, Output = na::SVector<T, N>>,
-    //     na::SVector<T, N>: std::ops::Add<Output = na::SVector<T, N>>,
-    // {
-    //     let func = |func_x: &na::SVector<T, N>, func_t: T| -> na::SVector<T, N> {
-    //         let linear_model = self.calculate_jacobian(func_x, u, func_t);
-    //         // x_dot = Ax + Bu
-    //         // TODO: the correct formula is something dx_dot = A* dx + B du
-    //         // since the jacobian is derivative relative to the state and inputs.
-    //         // Would keeping track of previous states help?
-    //         // Integrating this would obtain dx and then add that to the current x?
-    //         linear_model.0 * *func_x + linear_model.1 * *u
-    //     };
-    //     integrator(&func, x, t, t + dt)
-    // }
 
     /// Derivative function x_dot = f(x, u, t) which is a function of
     /// the system's current state, control input, and time
